9fcb103f4f60625f40366a46e51ccda1c7cc79a3
SafariTsai <safaritsai@gmail.com>
2025-04-09 11:11:58 +0800

    Update index.ts

--- web/config/index.ts
+++ web/config/index.ts
@@ -24,9 +24,9 @@ else {
   // const domainParts = globalThis.location?.host?.split('.');
   // in production env, the host is dify.app . In other env, the host is [dev].dify.app
   // const env = domainParts.length === 2 ? 'ai' : domainParts?.[0];
-  apiPrefix = 'http://localhost:5001/console/api'
-  publicApiPrefix = 'http://localhost:5001/api' // avoid browser private mode api cross origin
-  marketplaceApiPrefix = 'http://localhost:5002/api'
+  apiPrefix = 'https://dify.zqfw.site/console/api'
+  publicApiPrefix = 'https://dify.zqfw.site/api' // avoid browser private mode api cross origin
+  marketplaceApiPrefix = 'https://dify.zqfw.site/api'
 }
 
 if (process.env.NEXT_PUBLIC_MARKETPLACE_API_PREFIX && process.env.NEXT_PUBLIC_MARKETPLACE_URL_PREFIX) {
